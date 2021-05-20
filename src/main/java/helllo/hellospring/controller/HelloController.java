3e4d5b38a66ea55d5cad63a31fb7ca2cc09bcb7e
juhyeon1114 <juhyeon1114@gmail.com>
2021-05-20 17:58:57 +0900

    init

---
+++ src/main/java/helllo/hellospring/controller/HelloController.java
@@ -0,0 +1,49 @@
+package helllo.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello"; // 타겟팅된 템플릿 이름: resources/templates/hello.html
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // response의 body에 return되는 값을 넣어주겠다는 의미
+    public String helloString(@RequestParam(value = "name", required = false) String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
